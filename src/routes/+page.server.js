5d490cf286877eb07e661fb5964b5899930c4754
Nicholas Fang <nicholas.fang+github@amplifyasian.com>
2023-09-12 01:27:23 -0500

    Connecting postgres database and creating tables.

---
+++ src/routes/+page.server.js
@@ -0,0 +1,65 @@
+import { createPool, sql } from '@vercel/postgres';
+
+async function seed() {
+  const createUserTable = await sql`
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      email VARCHAR(255) UNIQUE NOT NULL,
+      "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    );
+  `;
+  const createTransactionsTable = await sql`
+    CREATE TYPE transaction_type as ENUM ('save', 'spend', 'donate');
+    CREATE TABLE IF NOT EXISTS transactions (
+      id SERIAL PRIMARY KEY,
+      user SERIAL REFERENCES users(id),
+      date DATE,
+      description VARCHAR(255),
+      category VARCHAR(255),
+      type transaction_type,
+      "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    );
+  `;
+  const createTotalsTable = await sql`
+    CREATE TABLE IF NOT EXISTS transactions (
+      id SERIAL PRIMARY KEY,
+      user SERIAL REFERENCES users(id),
+      save INTEGER,
+      spend INTEGER,
+      donate INTEGER,
+      "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    );
+  `;
+  return {
+    createUserTable,
+    createTransactionsTable,
+    createTotalsTable
+  }
+}
+
+export async function load() {
+  const db = createPool();
+  const startTime = Date.now();
+  try {
+    const { rows: users } = await db.query('SELECT * FROM users');
+    const duration = Date.now() - startTime;
+    return {
+      users: users,
+      duration: duration,
+    };
+  } catch (error) {
+    if (error?.message === `relation "users" does not exist`) {
+      console.log("Table does not exist, creating and seeding it with dummy data now...");
+      await seed();
+      const { rows: users } = await db.query('SELECT * FROM users');
+      const duration = Date.now() - startTime;
+      return {
+        users: users,
+        duration: duration
+      };
+    } else {
+      throw error;
+    }
+  }
+}
---
+++ src/routes/transactions/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+  import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, Checkbox, TableSearch } from 'flowbite-svelte';
+</script>
+
+<div class="table-container">
+  <Table color="yellow" divClass="relative overflow-x-auto">
+    <TableHead>
+      <TableHeadCell>Product name</TableHeadCell>
+      <TableHeadCell>Color</TableHeadCell>
+      <TableHeadCell>Category</TableHeadCell>
+      <TableHeadCell>Price</TableHeadCell>
+    </TableHead>
+    <TableBody class="divide-y">
+      <TableBodyRow>
+        <TableBodyCell>Apple MacBook Pro 17"</TableBodyCell>
+        <TableBodyCell>Sliver</TableBodyCell>
+        <TableBodyCell>Laptop</TableBodyCell>
+        <TableBodyCell>$2999</TableBodyCell>
+      </TableBodyRow>
+      <TableBodyRow>
+        <TableBodyCell>Microsoft Surface Pro</TableBodyCell>
+        <TableBodyCell>White</TableBodyCell>
+        <TableBodyCell>Laptop PC</TableBodyCell>
+        <TableBodyCell>$1999</TableBodyCell>
+      </TableBodyRow>
+      <TableBodyRow>
+        <TableBodyCell>Magic Mouse 2</TableBodyCell>
+        <TableBodyCell>Black</TableBodyCell>
+        <TableBodyCell>Accessories</TableBodyCell>
+        <TableBodyCell>$99</TableBodyCell>
+      </TableBodyRow>
+    </TableBody>
+    <tfoot>
+      <tr class="font-semibold text-gray-900 dark:text-white">
+        <th scope="row" class="py-3 px-6 text-base">Total</th>
+        <td class="py-3 px-6">3</td>
+        <td class="py-3 px-6">21,000</td>
+      </tr>
+    </tfoot>
+  </Table>
+</div>
