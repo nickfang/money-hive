a7e0b4665848375049696292a6a5cc4cc4ad372c
Alex Martinez <alexmtz298@gmail.com>
2023-10-02 14:24:08 -0500

    Remove class

--- src/routes/transactions/+page.svelte
+++ src/routes/transactions/+page.svelte
@@ -96,7 +96,7 @@
 	}
 
 	const inputClasses =
-		'py-1.5 px-3 block w-full rounded-md border-0 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6';
+		'py-1.5 px-3 block rounded-md border-0 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6';
 
 	const alignInputWithText = '-ml-3';
 </script>
